eeaaa374319d0bbf34cc842ee75387e71ae1a63d
dissko <danberube@gmail.com>
2025-11-22 11:25:02 -0500

    Update svelte.config.js
    removed msx

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
 
 
 /** @type {import('@sveltejs/kit').Config} */
