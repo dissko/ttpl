e71d789e33f3539cffdefe6c426ab258e4b97e22
Dan Berube <danberube@gmail.com>
2025-11-24 11:21:23 -0500

    added post

--- src/lib/TrafficCounter.svelte
+++ src/lib/TrafficCounter.svelte
@@ -6,13 +6,10 @@
   let content = 'C';
   let addend = 0;
   let resave = 'S';
+  let again = "N";
 
 
   onMount(async () => {
-    const repoOwner = import.meta.env.VITE_GIT_REPO_OWNER;
-    const repoName = import.meta.env.VITE_GIT_REPO_NAME;
-    const filePath = import.meta.env.VITE_GIT_FILE_PATH;
-    const accessToken = import.meta.env.VITE_GIT_ACCESS_TOKEN;
     const response = await fetch(
         'https://api.github.com/repos/nicksalt/ttpl/contents/src/traffic/map-count-0.md',
         {
@@ -40,44 +37,52 @@
         error = 'F'
     }
     
-    const responseagain = await fetch('https://api.github.com/repos/nicksalt/ttpl/contents/src/traffic/map-count-0.md', {
-        method: 'POST',
-        body: JSON.stringify({resave}),
-        headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer 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`,
-    },
+    const responseagain = await fetch(
+        'https://api.github.com/repos/nicksalt/ttpl/contents/src/traffic/map-count-0.md', {
+            method: 'POST',
+            body: JSON.stringify({resave}),
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer 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`,
+            },
+    });
+    if (responseagain.ok) {
+        again = "N";
+
+    } else {
+        console.error('Failed to save json:', response.status);
+        again = 'F'
+    }
+
     
+}
     
-    
-    
-    
-  });
 
   async function increment() {
     count++;
-    const repoOwner = import.meta.env.VITE_GIT_REPO_OWNER;
-    const repoName = import.meta.env.VITE_GIT_REPO_NAME;
-    const filePath = import.meta.env.VITE_GIT_FILE_PATH;
-    const accessToken = import.meta.env.VITE_GIT_ACCESS_TOKEN;
-    await fetch('https://api.github.com/repos/nicksalt/ttpl/contents/src/traffic/map-count-0.md', {
+    const responseonceagain = await fetch('https://api.github.com/repos/nicksalt/ttpl/contents/src/traffic/map-count-0.md', {
       method: 'POST',
       body: JSON.stringify({resave}),
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer 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`,
       },
-    
-    
-    
-    
-    
     });
+        if (responseonceagain.ok) {
+        again = "N";
+
+    } else {
+        console.error('Failed to save json:', response.status);
+        again = 'F'
+    }
+    
+    
+    
   }
 </script>
 
 <div class="traffic-counter">
-  <h1>Traffic Count: {count}{error}{repo}{content}{addend}{resave}</h1>
+  <h1>Traffic Count: {count}{error}{repo}{content}{addend}{resave}{again}</h1>
   <button on:click={increment}>Increment</button>
 </div>
 
