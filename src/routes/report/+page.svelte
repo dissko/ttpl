0b26630b19dda6f3bd1c88e7e7c9fcca376e5836
Dan Berube <danberube@gmail.com>
2026-03-19 15:33:39 -0400

    added report page

---
+++ src/routes/report/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+	import PageHeader from '$lib/components/PageHeader.svelte';
+	import SvelteSeo from 'svelte-seo';
+</script>
+
+<SvelteSeo
+	title="Library Calendar - Tyendinaga Township Public Library"
+	description="Stay up to date with our events and programs."
+	canonical="https://ttpl.ca/calendar"
+	keywords="tyendinaga, shannonville, belleville, ontario, library, books, events, community, resources"
+/>
+
+<main class="bg-base-100">
+	<!-- Hero Section -->
+	<PageHeader title="Calendar" subtitle="Stay up to date with our events and programs." />
+
+	<!-- Calendar Embed -->
+	<section class="px-4 py-10">
+        <div>
+            <p class="mt-auto text-xs">
+                <img src="calendar/MonthlyCalendars.jpg" alt="This Month's Calendar">
+            </p>
+ 
+ 
+ 
+		</div>
+	</section>
+</main>
