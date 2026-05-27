6b83f84929a0257d3975f7dd26de4cbe0580e50d
Dan Berube <danberube@gmail.com>
2026-05-27 09:56:21 -0400

    spelling

--- src/routes/board/+page.svelte
+++ src/routes/board/+page.svelte
@@ -4,7 +4,7 @@
 	import LibraryPolicies from '$lib/components/board/LibraryPolicies.svelte';
 	import MeetingMinutes from '$lib/components/board/MeetingMinutes.svelte';
 	import MeetingAgendas from '$lib/components/board/MeetingAgendas.svelte';
-    import ceoMeetings from '$lib/components/board/CeoReports.svelte';
+    import ceoReports from '$lib/components/board/CeoReports.svelte';
 
 	import SvelteSeo from 'svelte-seo';
 
